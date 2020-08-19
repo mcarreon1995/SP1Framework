3c1e4eee90f0d3d0f1332f7d2693b54d53c95327
number10203 <69341783+number10203@users.noreply.github.com>
2020-08-19 14:58:27 +0800

    Update Entity.cpp

--- SP1Framework/Entity.cpp
+++ SP1Framework/Entity.cpp
@@ -22,16 +22,8 @@ int Entity::getposY() {
 	return myPos.getYpos();
 }
 void Entity::setposX(int X) {
-	if (X > 19)
-		X = 19;
-	else if (X < 0)
-		X = 0;
 	myPos.setXpos(X);
 }
 void Entity::setposY(int Y) {
-	if (Y > 19)
-		Y = 19;
-	else if (Y < 0)
-		Y = 0;
 	myPos.setYpos(Y);
 }

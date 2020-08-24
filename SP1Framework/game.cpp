26fae217150c75c5f843c464e41c9ea665709354
Mark Albert Carreon <mark.albert1995@gmail.com>
2020-08-24 15:18:31 +0800

    color samples removed
    removed the colour samples from the background

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -354,15 +354,7 @@ void renderGame()
 
 void renderMap()
 {
-
-    // Set up sample colours, and output shadings
-    const WORD colors[] = {
-        0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
-        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6, 0, 0x434343
-    };
-
-    COORD c;
-    for (int i = 0; i < 12; ++i)
+    /*for (int i = 0; i < 12; ++i)
     {
         c.X = 5 * i;
         c.Y = i + 1;
@@ -375,7 +367,17 @@ void renderMap()
             c.Y = j;
             g_Console.writeToBuffer(c, " ", colors[13]);
         }
-    }
+    }*/
+
+
+    // Set up sample colours, and output shadings
+    const WORD colors[] = {
+        0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
+        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6, 0, 0x434343
+    };
+
+    COORD c;
+
     for (int i = 0; i < 50; i++) {
         for (int j = 0; j < 25; j++) {
             c.X = 15 + i;
